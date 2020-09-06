b189e9d3cc54724581ce30010b8efc4a1697f4e4
Max-Mexanik <2202max@bk.ru>
2020-09-06 21:57:59 +0300

    Добавленно Четвертое задание

--- src/ru/maxmexanik/java2020/Main.java
+++ src/ru/maxmexanik/java2020/Main.java
@@ -1,9 +1,14 @@
 package ru.maxmexanik.java2020;
 
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
 public class Main {
     static int[] Numbers = new int[] {1,2,3,4,5,6,7,8,9};
+    static int[] RandomMass = new int[16];
     static int Sum, A;
-    static double GarmSum = 0;
+    static double GarmSum;
 
 
     public static void main(String[] args) {
@@ -16,9 +21,8 @@ public class Main {
         System.out.println(Sum);
         Sum = 0;
 
-        while(A < Numbers.length)
-        {
-            Sum+=Numbers[A];
+        while (A < Numbers.length) {
+            Sum += Numbers[A];
             A++;
         }
         System.out.print("Cумма полученная сумированием в цикле while: ");
@@ -27,7 +31,7 @@ public class Main {
         A = 0;
 
         do {
-            Sum+=Numbers[A];
+            Sum += Numbers[A];
             A++;
         }
         while (A < Numbers.length);
@@ -36,18 +40,48 @@ public class Main {
 
         //Второе задание, вывод аргументов командной строки на экран в цикле for
         System.out.print("Аргументы командной строки: ");
-        for(String str : args)
-        {
+        for (String str : args) {
             System.out.print(str);
         }
         System.out.println();
         //Третье задание, вывод 10ти чисел гармонического ряда на экран
         System.out.println("Первые десять элементов гармонического ряда: ");
-        for(int s = 1; s<=10; s++)
-        {
-            GarmSum += 1./s;
+        for (int s = 1; s <= 10; s++) {
+            GarmSum += 1. / s;
             System.out.println(GarmSum);
         }
+        //Четвертое задание, генерация и сортировка массива из случайных целых чисел
+        //Заполняем массив случайными значениями
+        for(int Z = 0; Z < RandomMass.length; Z++)
+        {
+            RandomMass[Z] = (int)Math.round((Math.random() * RandomMass.length));
+        }
+        System.out.println("Неотсортиорванный массив: ");
+        for (int Num: RandomMass) {
+            System.out.println(Num);
+        }
 
+        BubbleSort(RandomMass);
+
+        System.out.println("Отсортиорванный массив: ");
+        for (int Num: RandomMass) {
+            System.out.println(Num);
+        }
+    }
+
+    public static void BubbleSort(int[] Array) {
+        boolean Sorted = false;
+        int Temp;
+        while(!Sorted) {
+            Sorted = true;
+            for (int i = 0; i < Array.length - 1; i++) {
+                if (Array[i] > Array[i+1]) {
+                    Temp = Array[i];
+                    Array[i] = Array[i+1];
+                    Array[i+1] = Temp;
+                    Sorted = false;
+                }
+            }
+        }
     }
 }
