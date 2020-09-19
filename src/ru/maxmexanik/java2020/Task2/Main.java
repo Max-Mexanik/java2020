27721e2cdb3a7731623401038aeb8799c2bf4a6e
Max-Mexanik <2202max@bk.ru>
2020-09-19 10:03:29 +0300

    Вторая практическая работа по JAVA

--- src/ru/maxmexanik/java2020/Task2/Main.java
+++ src/ru/maxmexanik/java2020/Task2/Main.java
@@ -1,6 +1,15 @@
 package ru.maxmexanik.java2020.Task2;
 
-public class Main {
-    Shape sh = new Shape(10, "2");
 
+
+public class Main
+{
+    public static void main(String[] args) {
+        Shape shape = new Shape(12, "Lol");
+        int count = shape.getSidesCount();
+        System.out.println(count);
+        shape.setSidesCount(45);
+        System.out.println(shape.getSidesCount());
+        System.out.println(shape);
+    }
 }
