39930a04e02be32de1804243ba9f278eb0d69a21
Max-Mexanik <2202max@bk.ru>
2020-09-19 11:37:38 +0300

    Вторая практическая работа по JAVA

--- src/ru/maxmexanik/java2020/Task2/TestBook.java
+++ src/ru/maxmexanik/java2020/Task2/TestBook.java
@@ -5,10 +5,16 @@ public class TestBook
     public static void main(String[] args)
     {
         Book d1 = new Book("Fahrenheit 451",199 ,35 );
-        Book d2 = new Book("Harry Potter",320 ,20 );
-        Book d3 = new Book("Harry Potter",110 ,50 );
-        Book d4 = new Book("Harry Potter",370 ,90 );
+        Book d2 = new Book("Crime and Punishment",320 ,20 );
+        Book d3 = new Book("Idiot",100 ,49 );
+        Book d4 = new Book("War and Peace",370 ,90 );
         System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
         d1.IntoDiscount();
+        d2.IntoDiscount();
+        d3.IntoDiscount();
+        d4.IntoDiscount();
     }
 }
