b06404909cb38d82ad12a28df1b3105191a592f6
Max-Mexanik <2202max@bk.ru>
2020-09-19 09:52:07 +0300

    Temp

---
+++ src/ru/maxmexanik/java2020/Task2/Main.java
@@ -0,0 +1,6 @@
+package ru.maxmexanik.java2020.Task2;
+
+public class Main {
+    Shape sh = new Shape(10, "2");
+
+}
---
+++ src/ru/maxmexanik/java2020/Task2/Shape.java
@@ -0,0 +1,31 @@
+package ru.maxmexanik.java2020.Task2;
+
+public class Shape {
+    private int NumOfSides;
+    private String ShapeName;
+
+    public Shape(int NumOfSides, String ShapeName) {
+        this.NumOfSides = NumOfSides;
+        this.ShapeName = ShapeName;
+    }
+
+    public void setSidesCount(int NumOfSides) {
+        this.NumOfSides = NumOfSides;
+    }
+
+    public int getSidesCount() {
+        return NumOfSides;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + ShapeName + '\'' +
+                '}';
+    }
+
+    public void printShapeInfo() {
+        System.out.println(NumOfSides);
+        System.out.println(ShapeName);
+    }
+}
